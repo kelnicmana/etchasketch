4edb2228ccc62670c93e8788627fdc21246b785a
Kellie Manasco <kellie.manasco@gmail.com>
2021-02-17 11:18:12 -0500

    now adds class of black to divs on mouseover. Also added shadows and adjusted UI design

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let pixelsInput = 2;
+let pixelsArea;
+
+addPixel();
 
 /// adds divs
 function addPixel() {
     pixelsArea = pixelsInput * pixelsInput;
     for (i = 0; i < pixelsArea; i++) {
-        const container = document.querySelector("#container");
-        const pixel = document.createElement("div");
+        let container = document.querySelector("#container");
+        let pixel = document.createElement("div");
         pixel.classList.add("pixel");
         container.appendChild(pixel);
     }
+    let black = document.querySelectorAll(".pixel");
+
+    for (let i = 0; i < black.length; i++) {
+        black[i].addEventListener("mouseover", function() {
+            black[i].classList.add("black");
+        });
+    }
+}
+
+/// resets/clears divs before adding more
+function resetPixels() {
+    for (i = 0; i < pixelsArea; i++) {
+        let clear = document.querySelector(".pixel");
+        clear.remove();
+    }
+    expandGrid();
 }
 
 /// expands the grid
@@ -24,12 +43,5 @@ function expandGrid() {
     }
 }
 
-
-/// event listeners for buttons
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", addPixel);
-
 const expand = document.querySelector("#expand");
-expand.addEventListener("click", expandGrid);
-
-
+expand.addEventListener("click", resetPixels);
