b37de8481cf33d4dd416f7d0694b2283913f90f1
Kellie Manasco <kellie.manasco@gmail.com>
2021-02-19 17:36:15 -0500

    Adds Bootstrap range slider for cross browser consistency, and cleaned up some code. No major changes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let pixelsArea;
-var colorSelection = document.getElementById("color").value;
-var randomColors;
-var gridSize = document.getElementById("gridSize").value;
+let colorSelection = document.getElementById("color").value;
+let randomColors;
+let gridSize = document.getElementById("gridSize").value;
 let rainbow = false;
-var checkbox = document.querySelector("input[name=rainbowCheck]");
+const checkbox = document.querySelector("input[name=rainbowCheck]");
 
 checkbox.addEventListener('change', function() {
   if (this.checked) {
@@ -36,7 +36,7 @@ resetPixels();
 /// resets/clears divs before adding more
 function resetPixels() {
     for (i = 0; i < pixelsArea; i++) {
-        let clear = document.querySelector(".pixel");
+        const clear = document.querySelector(".pixel");
         clear.remove();
     }
     expandGrid();
@@ -61,7 +61,7 @@ function addPixel() {
     let black = document.querySelectorAll(".pixel");
 
     /// helps prevents mobile devices from accidentally scrolling during use
-    this.container.addEventListener("touchstart", function(e) {
+    container.addEventListener("touchstart", function(e) {
         if (e.touches.length == 1) {
             e.preventDefault();
         }
@@ -69,9 +69,9 @@ function addPixel() {
 
     /// allows mobile gestures to work (adds black class on mobile/tablet)
     
-    this.container.addEventListener("touchmove", function(e) {
-        var touch = e.touches[0];
-        var selected = document.elementFromPoint(touch.clientX, touch.clientY);
+    container.addEventListener("touchmove", function(e) {
+        let touch = e.touches[0];
+        let selected = document.elementFromPoint(touch.clientX, touch.clientY);
         if (selected) {
             if (rainbow === true) {
                 rainbowToggle();
@@ -83,7 +83,6 @@ function addPixel() {
         }
     });
     
-
     /// adds selected color on pc/laptop
     for (let i = 0; i < black.length; i++) {
         black[i].addEventListener("mouseover", function() {
